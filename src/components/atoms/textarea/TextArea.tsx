75e77fcc8f9107eaad0fd38c1e66539f10bb978b
kyonyhk <kuoloon@hotmail.com>
2024-06-06 16:29:24 +0800

    comment out textarea component

--- src/components/atoms/textarea/TextArea.tsx
+++ src/components/atoms/textarea/TextArea.tsx
@@ -1,5 +1,5 @@
-import { styled, HTMLStyledProps } from '@shadow/panda/styled-system/jsx';
-import { textarea } from '@shadow/panda/styled-system/recipes';
+// import { styled, HTMLStyledProps } from '@shadow/panda/styled-system/jsx';
+// import { textarea } from '@shadow/panda/styled-system/recipes';
 
-export const TextArea = styled('textarea', textarea);
-export type TextAreaProps = HTMLStyledProps<typeof TextArea>;
+// export const TextArea = styled('textarea', textarea);
+// export type TextAreaProps = HTMLStyledProps<typeof TextArea>;
